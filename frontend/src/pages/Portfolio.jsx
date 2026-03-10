c7161ed030750c1929708539044af5d0dce3c6d5
Nuren7 <nurenislam01@gmail.com>
2026-03-10 13:57:01 +0100

    v1Portfolio initial commit

--- frontend/src/pages/Portfolio.jsx
+++ frontend/src/pages/Portfolio.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import PortfolioCard from '../components/portfolio/PortfolioCard'
+
+
 function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <div>
+      <PortfolioCard />
+
+    </div>
   )
 }
 
