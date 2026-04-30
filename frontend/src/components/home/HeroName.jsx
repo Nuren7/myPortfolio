f516f1eadf68d659f1360e6fa3cca4503f70a582
Nuren7 <nurenislam01@gmail.com>
2026-04-30 18:37:52 +0100

    figuring out hero image placement and size for mobile, also adjusting hero title size for better responsiveness

--- frontend/src/components/home/HeroName.jsx
+++ frontend/src/components/home/HeroName.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeroName() {
   return (
-    <div className='overflow-hidden w-full bg-transparent fixed bottom-20 sm:bottom-46 md:bottom-0 left-0 z-0 animate-slide-In-Up-Slow'>
+    <div className='overflow-hidden w-full bg-transparent fixed bottom-40 sm:bottom-80 md:bottom-0 left-0 z-0 animate-slide-In-Up-Slow'>
       <div className='flex w-max whitespace-nowrap animate-infiniteScroll'>
         <div className='flex text-stone-200 shrink-0 text-[1000%] gap-8 font-thin font-montserrat'>
           <span>Nuren Islam </span>
