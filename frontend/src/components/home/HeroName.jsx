dedb7ca89d708624929b43cd76368fb41a290a93
Nuren7 <nurenislam01@gmail.com>
2026-04-30 18:34:17 +0100

    style jsx

--- frontend/src/components/home/HeroName.jsx
+++ frontend/src/components/home/HeroName.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeroName() {
   return (
-    <div className='overflow-hidden w-full bg-transparent fixed bottom-0 left-0 z-0 animate-slide-In-Up-Slow'>
+    <div className='overflow-hidden w-full bg-transparent fixed bottom-20 sm:bottom-32 md:bottom-0 left-0 z-0 animate-slide-In-Up-Slow'>
       <div className='flex w-max whitespace-nowrap animate-infiniteScroll'>
         <div className='flex text-stone-200 shrink-0 text-[1000%] gap-8 font-thin font-montserrat'>
           <span>Nuren Islam </span>
